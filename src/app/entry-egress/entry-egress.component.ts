696d60e15f20ac4f547dcaca9ac42bb682dee0f9
Jonathan <jalcantara1990@gmail.com>
2018-09-25 20:24:22 +0200

    Structure defined

---
+++ src/app/entry-egress/entry-egress.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entry-egress',
+  templateUrl: './entry-egress.component.html',
+  styles: []
+})
+export class EntryEgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
